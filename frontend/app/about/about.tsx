199d32b5fdb742945af4e87d9556e9b5b11fab79
BengieVillesco <bvillesco@gmail.com>
2026-02-23 02:01:32 +0800

    Frontend: Clean to start over

--- frontend/app/about/about.tsx
+++ frontend/app/about/about.tsx
@@ -1,21 +1,9 @@
-import styles from "./About.module.css";
-import Picture from "../../public/ID.png";
-import Image from "next/image";
+// import styles from "./About.module.css";
 
 export default function About() {
   return (
-    <div className={styles.about}>
-      <section className={styles.profile}>
-        <Image src={Picture} alt="profile" />
-      </section>
-      <section className={styles.description}>
-        <h1>About Page!</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptates, quod, quia, quae voluptatem quibusdam.
-        </p>
-      </section>
-      {/* <h1>About Page!</h1> */}
-    </div>
+    <>
+      <h1>About Page!</h1>
+    </>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,33 +1,25 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import styles from "./Home.module.css";
+import Home from "./home/home";
 import About from "./about/about";
 import Project from "./projects/project";
 import Contact from "./contact/contact";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className={styles["page-layout"]}>
+    <div>
       <Navbar />
       <main>
-        <section className={styles["home-section"]} id="home">
-          <h1>Bengie Villesco</h1>
-          <h2>Frontend Developer</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptates, quod, quia, quae voluptatem quibusdam.
-          </p>
-          <a href="#project">
-            <button>View My Work</button>
-          </a>
+        <section id="home">
+          <Home />
         </section>
-        <section className={styles["about-section"]} id="about">
+        <section id="about">
           <About />
         </section>
-        <section className={styles["project-section"]} id="project">
+        <section id="project">
           <Project />
         </section>
-        <section className={styles["contact-section"]} id="contact">
+        <section id="contact">
           <Contact />
         </section>
         <Footer />
